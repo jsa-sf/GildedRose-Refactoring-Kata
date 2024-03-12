1569a36c66fea69da2c4dc767aa59b9c32d88dcf
Jérémy SAFONT <jrm.safont@gmail.com>
2024-03-12 22:50:03 +0100

    Add missing files versioning

--- php/src/GildedRose.php
+++ php/src/GildedRose.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace GildedRose;
 
+use GildedRose\Enum\Name;
+use GildedRose\Enum\Operation;
+
 final class GildedRose
 {
     /**
@@ -17,51 +20,79 @@ final class GildedRose
     public function updateQuality(): void
     {
         foreach ($this->items as $item) {
-            if ($item->name != 'Aged Brie' and $item->name != 'Backstage passes to a TAFKAL80ETC concert') {
-                if ($item->quality > 0) {
-                    if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                        $item->quality = $item->quality - 1;
-                    }
-                }
-            } else {
-                if ($item->quality < 50) {
-                    $item->quality = $item->quality + 1;
-                    if ($item->name == 'Backstage passes to a TAFKAL80ETC concert') {
-                        if ($item->sellIn < 11) {
-                            if ($item->quality < 50) {
-                                $item->quality = $item->quality + 1;
-                            }
-                        }
-                        if ($item->sellIn < 6) {
-                            if ($item->quality < 50) {
-                                $item->quality = $item->quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                $item->sellIn = $item->sellIn - 1;
-            }
-
-            if ($item->sellIn < 0) {
-                if ($item->name != 'Aged Brie') {
-                    if ($item->name != 'Backstage passes to a TAFKAL80ETC concert') {
-                        if ($item->quality > 0) {
-                            if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                                $item->quality = $item->quality - 1;
-                            }
-                        }
-                    } else {
-                        $item->quality = $item->quality - $item->quality;
-                    }
-                } else {
-                    if ($item->quality < 50) {
-                        $item->quality = $item->quality + 1;
-                    }
-                }
+            if (Name::tryFrom($item->name) !== Name::SULFURAS) {
+                $item = $this->updateSellInStep($item);
+                $item = $this->updateQualityStep($item);
             }
         }
+    }
+
+    private function updateQualityStep(Item $item): Item
+    {
+        $operation = Operation::DECREASE;
+        $value = 1;
+        switch (Name::tryFrom($item->name)) {
+            case Name::BACKSTAGE_PASSES:
+                if ($item->sellIn < 0) {
+                    $operation = Operation::RESET;
+                } else {
+                    $operation = Operation::INCREASE;
+                    if ($item->sellIn < 6) {
+                        $value = 3;
+                    } elseif ($item->sellIn < 11) {
+                        $value = 2;
+                    }
+                }
+                break;
+            case Name::AGED_BRIE:
+                $operation = Operation::INCREASE;
+                break;
+            case Name::CONJURED:
+                $value = 2;
+        }
+
+        return $this->changeQuality($item, $operation, $value);
+    }
+
+    private function updateSellInStep(Item $item): Item
+    {
+        $item->sellIn--;
+        return $item;
+    }
+
+    private function changeQuality(Item $item, Operation $operation, int $value): Item
+    {
+        return match ($operation) {
+            Operation::DECREASE => $this->decreaseQuality($item, $item->sellIn < 0 ? $value * 2 : $value),
+            Operation::INCREASE => $this->increaseQuality($item, $value),
+            Operation::RESET => $this->resetQuality($item)
+        };
+    }
+
+    private function decreaseQuality(Item $item, int $value): Item
+    {
+        if ($item->quality > 0) {
+            $newValue = $item->quality - $value;
+            $item->quality = $newValue < 0 ? 0 : $newValue;
+        }
+
+        return $item;
+    }
+
+    private function increaseQuality(Item $item, int $value = 1): Item
+    {
+        if ($item->quality < 50) {
+            $newValue = $item->quality + $value;
+            $item->quality = $newValue > 50 ? 50 : $newValue;
+        }
+
+        return $item;
+    }
+
+    private function resetQuality(Item $item): Item
+    {
+        $item->quality = 0;
+
+        return $item;
     }
 }
